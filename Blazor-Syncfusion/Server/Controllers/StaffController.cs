8f0b05ddda7c7c3d1f336136c2d1203d0a2b7c31
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-17 17:00:20 +0200

    Added create & edit employee

--- Blazor-Syncfusion/Server/Controllers/StaffController.cs
+++ Blazor-Syncfusion/Server/Controllers/StaffController.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorSyncfusion.Server.Data;
+using BlazorSyncfusion.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,50 @@ namespace BlazorSyncfusion.Server.Controllers
             return Ok(await _context.Employees
                 .Where(c => c.IsEmployee)
                 .ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetStaffById(int id)
+        {
+            var result = await _context.Employees.FindAsync(id);
+            if(result is null)
+            {
+                return NotFound("Staff not found.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateStaff(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<string>> UpdateStaff(int id, Employee employee)
+        {
+            var dbContact = await _context.Employees.FindAsync(id);
+            if (dbContact is null)
+            {
+                return NotFound("Staff not found.");
+            }
+
+            dbContact.FirstName = employee.FirstName;
+            dbContact.LastName = employee.LastName;
+            dbContact.NickName = employee.NickName;
+            dbContact.Title = employee.Title;
+            dbContact.Mail = employee.Mail;
+            dbContact.Phone = employee.Phone ?? dbContact.Phone;
+            dbContact.BirthDate = employee.BirthDate;
+            dbContact.DateLastUpdated = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
         }
     }
 }
